4ea78b0db8def1a1e67291f55dac63d6a7b4ed00
Niko Matsakis <niko@alum.mit.edu>
2017-03-28 15:31:34 -0400

    ok woot got something that seems like it's going to work

--- src/mock/mod.rs
+++ src/mock/mod.rs
@@ -59,7 +59,10 @@ pub struct CounterFields {
 impl CounterFields {
     pub fn new(f: u32) -> CounterFields {
         // user code here
-        CounterFields { GObject: GObjectFields::new(), count: Cell::new(f) }
+        CounterFields {
+            GObject: GObjectFields::new(),
+            count: Cell::new(f),
+        }
     }
 }
 
@@ -70,6 +73,7 @@ trait Counter: 'static {
 
     // instead:
     fn Counter(&self) -> &CounterFields;
+    fn CounterPtr(&self) -> Ptr<Counter>;
 
     fn add(&self, a: u32) -> u32;
 
@@ -85,28 +89,22 @@ fn upcast_Counter<T: ?Sized + Counter>(p: &Ptr<T>) -> &Ptr<Counter> {
     panic!("FIXME")
 }
 
-impl<T: Counter> CounterSuper for Ptr<T> {
+impl<T: ?Sized + Counter> CounterSuper for Ptr<T> {
     fn add(this: &Self, a: u32) -> u32 {
-        let this: Ptr<Counter> = this.clone();
-        CounterSuper::add(&this, a)
+        fn m(this: &Ptr<Counter>, a: u32) -> u32 {
+            let foo = this.Counter();
+            let v = foo.count.get() + a;
+            foo.count.set(v);
+            v
+        }
+        m(&this.CounterPtr(), a)
     }
 
     fn get(this: &Self) -> u32 {
-        let this: Ptr<Counter> = this.clone();
-        CounterSuper::get(&this)
-    }
-}
-
-impl CounterSuper for Ptr<Counter> {
-    fn add(this: &Self, a: u32) -> u32 {
-        let foo = this.Counter();
-        let v = foo.count.get() + a;
-        foo.count.set(v);
-        v
-    }
-
-    fn get(this: &Self) -> u32 {
-        this.Counter().count.get()
+        fn m(this: &Ptr<Counter>) -> u32 {
+            this.Counter().count.get()
+        }
+        m(&this.CounterPtr())
     }
 }
 
@@ -122,21 +120,26 @@ impl Counter {
                 &self.fields
             }
 
+            fn CounterPtr(&self) -> Ptr<Counter> {
+                self.self_ref
+                    .borrow()
+                    .upgrade()
+                    .unwrap()
+            }
+
             fn add(&self, a: u32) -> u32 {
-                let self_ref = self.self_ref.borrow().upgrade().unwrap();
-                CounterSuper::add(&self_ref, a)
+                CounterSuper::add(&self.CounterPtr(), a)
             }
 
             fn get(&self) -> u32 {
-                let self_ref = self.self_ref.borrow().upgrade().unwrap();
-                CounterSuper::get(&self_ref)
+                CounterSuper::get(&self.CounterPtr())
             }
         }
 
         let ptr = Ptr::new(Impl {
-            fields: CounterFields::new(f),
-            self_ref: RefCell::new(Weak::new())
-        });
+                               fields: CounterFields::new(f),
+                               self_ref: RefCell::new(Weak::new()),
+                           });
 
         {
             let mut self_ref = ptr.self_ref.borrow_mut();
@@ -149,79 +152,115 @@ impl Counter {
 
 ///////////////////////////////////////////////////////////////////////////
 // MultCounter
-//
-//pub struct MultCounterFields {
-//    CounterFields: CounterFields,
-//    multiplier: u32,
-//}
-//
-//impl MultCounterFields {
-//    pub fn new(m: u32) -> MultCounterFields {
-//        let CounterFields = CounterFields::new(0);
-//        MultCounterFields { CounterFields, multiplier: m }
-//    }
-//}
-//
-//trait MultCounter: Counter {
-//    fn MultCounter(&self) -> &MultCounterFields;
-//    fn multiplier(&self) -> u32;
-//}
-//
-//trait MultCounterSuper {
-//    fn multiplier(this: &Self) -> String;
-//    fn add(this: &Self, a: u32) -> u32;
-//    fn get(this: &Self) -> u32;
-//}
-//
-//impl<T: ?Sized + MultCounter> MultCounterSuper for Ptr<T> {
-//    fn multiplier(this: &Self) -> String {
-//        fn m(this: &Ptr<MultCounter>) -> u32 {
-//            this.MultCounter().multiplier
-//        }
-//
-//        MultCounterSuper::add(this)
-//    }
-//
-//    fn add(this: &Self, a: u32) -> u32 {
-//        fn m(this: &Ptr<MultCounter>, a: u32) -> u32 {
-//            CounterSuper::add(this, a * this.MultCounter().multiplier)
-//        }
-//    }
-//
-//    fn get(this: &Self) -> u32 {
-//        CounterSuper::get(this)
-//    }
-//}
-//
-//impl MultCounter {
-//    pub fn new(s: String) -> Ptr<MultCounter> {
-//        struct Impl(MultCounterFields);
-//
-//        impl MultCounter for Ptr<Impl> {
-//            fn MultCounter(&self) -> &MultCounterFields {
-//                &self.0
-//            }
-//
-//            fn multiplier(&self) -> u32 {
-//                MultCounterSuper::multiplier(self)
-//            }
-//        }
-//
-//        impl Counter for Ptr<Impl> {
-//            fn Counter(&self) -> &CounterFields {
-//                &self.0.FooFields
-//            }
-//
-//            fn add(&self, a: u32) -> u32 {
-//                MultCounterSuper::add(self, a)
-//            }
-//
-//            fn get(&self) -> u32 {
-//                MultCounterSuper::add(self)
-//            }
-//        }
-//
-//        Ptr::new(Impl(MultCounterFields::new(s)))
-//    }
-//}
-//
+
+pub struct MultCounterFields {
+    CounterFields: CounterFields,
+    multiplier: u32,
+}
+
+impl MultCounterFields {
+    pub fn new(m: u32) -> MultCounterFields {
+        let CounterFields = CounterFields::new(0);
+        MultCounterFields {
+            CounterFields,
+            multiplier: m,
+        }
+    }
+}
+
+trait MultCounter: Counter {
+    fn MultCounter(&self) -> &MultCounterFields;
+    fn MultCounterPtr(&self) -> Ptr<MultCounter>;
+    fn multiplier(&self) -> u32;
+}
+
+trait MultCounterSuper {
+    fn multiplier(this: &Self) -> u32;
+    fn add(this: &Self, a: u32) -> u32;
+    fn get(this: &Self) -> u32;
+}
+
+impl<T: ?Sized + MultCounter> MultCounterSuper for Ptr<T> {
+    fn multiplier(this: &Self) -> u32 {
+        fn m(this: &Ptr<MultCounter>) -> u32 {
+            this.MultCounter().multiplier
+        }
+
+        m(&this.MultCounterPtr())
+    }
+
+    fn add(this: &Self, a: u32) -> u32 {
+        fn m(this: &Ptr<MultCounter>, a: u32) -> u32 {
+            CounterSuper::add(this, a * this.MultCounter().multiplier)
+        }
+
+        m(&this.MultCounterPtr(), a)
+    }
+
+    fn get(this: &Self) -> u32 {
+        fn m(this: &Ptr<MultCounter>) -> u32 {
+            CounterSuper::get(this)
+        }
+
+        m(&this.MultCounterPtr())
+    }
+}
+
+impl MultCounter {
+    pub fn new(m: u32) -> Ptr<MultCounter> {
+        struct Impl {
+            fields: MultCounterFields,
+            self_ref: RefCell<Weak<Impl>>,
+        }
+
+        impl MultCounter for Impl {
+            fn MultCounter(&self) -> &MultCounterFields {
+                &self.fields
+            }
+
+            fn MultCounterPtr(&self) -> Ptr<MultCounter> {
+                self.self_ref
+                    .borrow()
+                    .upgrade()
+                    .unwrap()
+            }
+
+            fn multiplier(&self) -> u32 {
+                MultCounterSuper::multiplier(&self.MultCounterPtr())
+            }
+        }
+
+        impl Counter for Impl {
+            fn Counter(&self) -> &CounterFields {
+                &self.fields.CounterFields
+            }
+
+            fn CounterPtr(&self) -> Ptr<Counter> {
+                self.self_ref
+                    .borrow()
+                    .upgrade()
+                    .unwrap()
+            }
+
+            fn add(&self, a: u32) -> u32 {
+                MultCounterSuper::add(&self.MultCounterPtr(), a)
+            }
+
+            fn get(&self) -> u32 {
+                MultCounterSuper::get(&self.MultCounterPtr())
+            }
+        }
+
+        let ptr = Ptr::new(Impl {
+                               fields: MultCounterFields::new(m),
+                               self_ref: RefCell::new(Weak::new()),
+                           });
+
+        {
+            let mut self_ref = ptr.self_ref.borrow_mut();
+            *self_ref = Arc::downgrade(&ptr);
+        }
+
+        ptr
+    }
+}
