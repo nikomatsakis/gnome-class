e9d8ae5f576b56722b2f1f27e45000d0b86e4425
Federico Mena Quintero <federico@gnome.org>
2017-08-22 15:59:37 -0500

    tests/signals.rs: New test to see that we generate the correct signals
    For now we just test the presence of the "value-changed" signal we
    create.  We are not able to generate the correct signal signature yet.

---
+++ tests/signals.rs
@@ -0,0 +1,63 @@
+#![feature(proc_macro)]
+
+// FIXME: remove the need for this
+#[macro_use] extern crate lazy_static;
+
+// FIXME: can we combine these two?
+extern crate gnome_class_shims;
+extern crate gobject_gen;
+
+use gobject_gen::gobject_gen;
+use gnome_class_shims::gobject_sys;
+use gnome_class_shims::GInstance;
+use std::cell::Cell;
+use std::ffi::CStr;
+use std::mem;
+use std::slice;
+
+gobject_gen! {
+    class Signaler {
+        struct SignalerPrivate {
+            val: Cell<u32>
+        }
+
+        signal value_changed(&self);
+
+        fn set_value(&self, v: u32) {
+            let private = self.private();
+            private.val.set(v);
+            // private.emit_value_changed();
+        }
+
+        fn get_value(&self) -> u32 {
+            let private = self.private();
+            private.val.get()
+        }
+    }
+}
+
+#[test]
+fn has_value_changed_signal() {
+    let obj: Signaler = Signaler::new();
+    let obj_type = Signaler::get_type();
+
+    unsafe {
+        let mut n_ids: libc::c_uint = mem::zeroed();
+
+        let raw_signal_ids = gobject_sys::g_signal_list_ids(obj_type, &mut n_ids);
+        assert_eq!(n_ids, 1);
+
+        let n_ids = n_ids as usize;
+
+        let signal_ids = slice::from_raw_parts(raw_signal_ids, n_ids);
+
+        let mut query: gobject_sys::GSignalQuery = mem::zeroed();
+        gobject_sys::g_signal_query(signal_ids[0], &mut query);
+
+        assert_eq!(query.itype, obj_type);
+        assert_eq!(query.signal_id, signal_ids[0]);
+
+        let signal_name = CStr::from_ptr(query.signal_name);
+        assert_eq!(signal_name.to_str().unwrap(), "value-changed");
+    }
+}
