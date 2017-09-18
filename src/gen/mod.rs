12014fb05cbeb74e1f4f0c87dd5e27793ffb8dd8
Federico Mena Quintero <federico@gnome.org>
2017-09-18 14:38:14 -0500

    Import the calling module into our mod so the prototypes have access

--- src/gen/mod.rs
+++ src/gen/mod.rs
@@ -187,6 +187,10 @@ impl<'ast> ClassContext<'ast> {
             use std::mem;
             use std::mem::transmute;
 
+            // Bring in our parent's stuff so the user's implementation
+            // can use what they had already defined there.
+            use super::*;
+
             // #[cfg(feature = "bindings")]
             // mod ffi;
 
