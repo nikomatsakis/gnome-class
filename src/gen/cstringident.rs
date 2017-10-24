bf85ba609edf52d70d1e04b5c53f5dea8be5614b
Federico Mena Quintero <federico@gnome.org>
2017-10-24 10:14:09 -0500

    CStrIdent: a little documentation

--- src/gen/cstringident.rs
+++ src/gen/cstringident.rs
@@ -1,7 +1,12 @@
 use quote::{Tokens, ToTokens};
 use syn::Ident;
 
-struct CStringIdent(Ident);
+/// Wraps an `Ident` so it can be tokenized as a C-friendly string literal.
+///
+/// For example, if the ident holds the name Foo, then this will
+/// generate "`b"Foo\0" as *const u8 as *const i8`", which is suitable
+/// for calling C APIs.
+pub struct CStringIdent(pub Ident);
 
 impl ToTokens for CStringIdent {
     fn to_tokens(&self, tokens: &mut Tokens) {
