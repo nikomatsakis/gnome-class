4da6fe8bdc42f5943854e6b78c9c4c2ab81f394a
Federico Mena Quintero <federico@gnome.org>
2017-11-03 13:27:19 -0600

    CStringIdent: reimplement in terms of Literal

--- src/gen/cstringident.rs
+++ src/gen/cstringident.rs
@@ -1,5 +1,6 @@
 use quote::{Tokens, ToTokens};
 use syn::Ident;
+use proc_macro2::{Literal, TokenNode};
 
 /// Wraps an `Ident` so it can be tokenized as a C-friendly string literal.
 ///
@@ -10,8 +11,15 @@ pub struct CStringIdent(pub Ident);
 
 impl ToTokens for CStringIdent {
     fn to_tokens(&self, tokens: &mut Tokens) {
-        tokens.append("b\"");
-        tokens.append(self.0.as_ref());
-        tokens.append("\\0\"");
+        // Make a b"Foo\0" byte literal
+
+        let s = self.0.as_ref();
+        let mut v = Vec::from (s.as_bytes());
+        v.push (0u8);
+        tokens.append (TokenNode::Literal(Literal::byte_string(&v)));
+
+        // Cast it to what glib expects
+
+        tokens.append_tokens (quote! { as *const u8 as *const i8 });
     }
 }
