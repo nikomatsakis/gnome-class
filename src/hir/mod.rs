fdf674df906a247c4ecfc19c7ff1ee13a40df4f4
Federico Mena Quintero <federico@gnome.org>
2017-11-08 15:54:07 -0600

    New hir::Program::from_ast_program() - start doing the conversion

--- src/hir/mod.rs
+++ src/hir/mod.rs
@@ -6,9 +6,14 @@
 // We construct this view of the world from the raw Abstract Syntax
 // Tree (AST) from the previous stage.
 
+use proc_macro2::{TokenStream};
 use syn::{Ident, ImplItem, Path};
+use synom::{Synom, SynomBuffer};
 
 use super::ast;
+use super::ast::get_program_classes;
+use super::checking::*;
+use super::errors::*;
 
 pub struct Program<'ast> {
     pub classes: Vec<Class<'ast>>
@@ -42,4 +47,76 @@ pub struct Method {
 pub struct Signal {
     // FIXME: signal flags
     pub item: ImplItem
+}
+
+impl<'ast> Program<'ast> {
+    pub fn from_ast_program(ast_program: ast::Program) -> Result<Program<'ast>> {
+        let ast_program = check_program(ast_program)?;
+
+        Ok(Program {
+            classes: Self::extract_classes (ast_program)
+        })
+    }
+
+    fn extract_classes(ast_program: ast::Program) -> Vec<Class<'ast>> {
+        let mut classes = Vec::new();
+
+        for ast_class in get_program_classes(&ast_program) {
+            classes.push (Self::extract_class (ast_class));
+        }
+
+        classes
+    }
+
+    fn extract_class(ast_class: &ast::Class) -> Class<'ast> {
+        Class {
+            name: ast_class.name.clone(),
+            superclass: ast_class.extends.clone().unwrap_or(make_path_glib_object()),
+            implements: Vec::new(),
+            instance_private: None,
+            slots: Vec::new()
+        }
+    }
+}
+
+fn make_path_glib_object() -> Path {
+    let tokens = quote! { glib::Object };
+    let token_stream = TokenStream::from(tokens);
+    let buffer = SynomBuffer::new(token_stream);
+    let cursor = buffer.begin();
+    Path::parse(cursor).unwrap().1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use quote::{ToTokens};
+
+    #[test]
+    fn creates_trivial_class() {
+        let raw = "class Foo {
+                       struct FooPrivate {
+                           foo: u32,
+                       }
+
+                       private_init () -> FooPrivate {
+                           FooPrivate {
+                               foo: 42,
+                           }
+                       }
+                   }";
+
+        let token_stream = raw.parse::<TokenStream>().unwrap();
+        let buffer = SynomBuffer::new(token_stream);
+        let cursor = buffer.begin();
+        let ast_program = ast::Program::parse(cursor).unwrap().1;
+
+        let program = Program::from_ast_program(ast_program).unwrap();
+
+        assert!(program.classes.len() == 1);
+
+        let class = &program.classes[0];
+        assert_eq!(class.name.as_ref(), "Foo");
+        assert_eq!(class.superclass.clone().into_tokens().to_string(), "glib :: Object");
+    }
 }
